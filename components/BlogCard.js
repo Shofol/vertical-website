7a9599523beaff12b30b355ca9e59468245f1ea3
Ian Jang <59151984+jangian@users.noreply.github.com>
2021-08-20 15:52:37 +0200

    image size

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -8,24 +8,21 @@ const BlogCard = ({ article }) => {
             <div className="w-full h-44 relative">
                 <img
                     src={`https://${article.fields.coverImage.fields.file.url}`}
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="center"
                     alt={'as'}
                     className="rounded-tl-3xl rounded-tr-3xl"
-                    quality={100}
+                    style={{ width: '384px', height: '176px', boxSizing: 'border-box' }}
                 />
                 <div className="absolute top-5 left-5 bg-vert-green text-white px-3 py-1 rounded-2xl">Category</div>
             </div>
             <div className={'w-16 h-16 -mt-7 ml-4 userImgPolygon'} style={{ background: `center / cover url(${article.fields.writerImage.fields.file.url}), #fff` }}></div>
-            <div className="px-7 py-5">
-                <div className="pr-6">
+            <div className="px-7 py-5 z-20">
+                <div className="pr-6 z-20">
                     <h4 className="mb-6">{article.fields.title}</h4>
                     <p>{`${article.fields.excerpt.substring(0, 100)}...`}</p>
                     <a href={`/blog/articles/${article.fields.slug}`} passHref className="flex items-center my-6"><div className="w-3 h-px bg-vert-blue mr-2"></div>Read more</a>
                 </div>
 
-                <div className="w-full bg-vert-green-light" style={{ height: '2px' }}></div>
+                <div className="w-full bg-vert-green-light z-20" style={{ height: '2px' }}></div>
                 <div className="flex items-center text-black my-6 text-sm">
                     <p>{article.fields.writer}</p>
                     <div className="h-1 w-1 rounded-full bg-black mx-2"></div>
