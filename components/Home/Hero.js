317b1ea6c4fd1a30f81c1abcf76b7ea6cf049f39
Marko Jakic <jakicmeister@gmail.com>
2021-09-15 15:02:54 +0200

    correct names hero

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -18,9 +18,9 @@ const Hero = ({ home }) => {
           <span className="text-vert-green font-bold">We help</span>
           <div className="img-rect mx-10" style={user1Bg}>
             <div className="description px-3 flex flex-col justify-center text-white absolute inset-0 w-full h-full bg-black bg-opacity-75">
-              <p className="text-tiny leading-none lg:text-base font-semibold">Scott Arden-Jones</p>
-              <p className="text-tiny leading-none lg:text-sm">Head of UK Engineering</p>
-              <p className="text-tiny leading-none lg:text-sm font-semibold">@Truepill</p>
+              <p className="text-tiny leading-none lg:text-base font-semibold">Carmen Spoida</p>
+              <p className="text-tiny leading-none lg:text-sm">Head of People &amp; Culture</p>
+              <p className="text-tiny leading-none lg:text-sm font-semibold">@Vertrical</p>
             </div>
           </div>
           <span className="mr-2 lg:mr-4 my-4 lg:my-0 text-vert-green font-bold">you craft</span>
@@ -30,17 +30,17 @@ const Hero = ({ home }) => {
           <div className="userImgPolygon w-44 h-44  lg:ml-10 relative img-polygon" style={user2Bg}>
             <div className="userImgPolygonOutline"></div>
             <div className="description-poly px-5 flex flex-col justify-center items-center text-white absolute inset-0 w-full h-full bg-black bg-opacity-75">
-              <p className="text-tiny leading-none lg:text-base text-center font-semibold">Scott Arden-Jones</p>
-              <p className="text-tiny leading-none lg:text-sm text-center">Head of UK Engineering</p>
-              <p className="text-tiny leading-none lg:text-sm font-semibold">@Truepill</p>
+              <p className="text-tiny leading-none lg:text-base text-center font-semibold">Omar Khan</p>
+              <p className="text-tiny leading-none lg:text-sm text-center">Head of Risk &amp; Compliance</p>
+              <p className="text-tiny leading-none lg:text-sm font-semibold">@Vertrical</p>
             </div>
           </div>
 
           <div style={{ flexBasis: '100%' }}></div>
           <div className="w-20 lg:w-40 h-20 lg:h-40 rounded-full bg-white mr-4 lg:mr-10 relative icon-circle" style={user3Bg}>
             <div className="description-circle px-3 flex flex-col justify-center items-center text-white absolute inset-0 w-full h-full bg-black bg-opacity-75">
-              <p className="text-tiny leading-none lg:text-base text-center font-semibold">Scott Arden-Jones</p>
-              <p className="text-tiny leading-none lg:text-sm text-center">Head of UK Engineering</p>
+              <p className="text-tiny leading-none lg:text-base text-center font-semibold">Sid Viswanathan</p>
+              <p className="text-tiny leading-none lg:text-sm text-center">Co-Founder &amp; President</p>
               <p className="text-tiny leading-none lg:text-sm font-semibold">@Truepill</p>
             </div>
           </div>
