d2e072505283c5a420ec0eb12b89aae63fc874c9
Ian Jang <59151984+jangian@users.noreply.github.com>
2021-09-16 17:51:55 +0200

    fmore granular queries

--- components/Home/Testimonial.js
+++ components/Home/Testimonial.js
@@ -5,7 +5,7 @@ const Testimonial = ({ text, employeeName, companyName, companyLink, post, index
     const user1Bg = useCoverBg(picture)
 
     return (
-        <div className="bg-white rounded-xl relative p-8 flex flex-col justify-between lg:h-43rem xl:h-112 2xl:h-22rem">
+        <div className="bg-white rounded-xl relative p-8 flex flex-col justify-between lg:h-43rem lg2:h-38rem lg3:h-33rem xl:h-112 xl2:h-26rem xl3:h-24rem 2xl:h-22rem ">
             <p className="text-vert-green text-xl font-bold">{`"${text}"`}</p>
             <div className="flex mt-6 items-center">
                 <div className={"w-16 h-16 relative " + (index === 1 ? 'rounded-2xl' : 'userImgPolygon')} style={user1Bg}>
