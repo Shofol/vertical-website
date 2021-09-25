1494864605568208ac91d4fcf1c8d85b4d0399a0
Marko Jakic <jakicmeister@gmail.com>
2021-09-25 17:47:33 +0200

    add preview domain

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -14,6 +14,7 @@ const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     environment: 'master',
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    host: 'preview.contentful.com',
 });
 
 
