ff82951b2bb7bb0eb0c612e1493f4ade2c60f494
shofol <jahananower@gmail.com>
2021-10-21 17:09:57 +0600

    modify social link and author

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import styles from '../styles/content.module.css';
 
 const BlogCard = ({ article }) => {
-
+    console.log(article);
     return (
         <div className="rounded-3xl max-w-xs lg:max-w-sm bg-white flex flex-col flex-none mb-6">
             <div className="w-full h-44 relative">
@@ -14,16 +14,16 @@ const BlogCard = ({ article }) => {
                 />}
                 <div className="absolute top-5 left-5 bg-vert-green text-white px-3 py-1 rounded-2xl">Category</div>
             </div>
-            {article.fields.writerImage && <div className={'w-16 h-16 -mt-7 ml-4 userImgPolygon'} style={{ background: `center / cover url(${article.fields.writerImage.fields.file.url}), #fff` }}></div>}
+            {article.fields.author.fields.image.fields.file.url && <div className={'w-16 h-16 -mt-7 ml-4 userImgPolygon'} style={{ background: `center / cover url(${article.fields.author.fields.image.fields.file.url}), #fff` }}></div>}
             <div className="px-7 py-5 z-20">
                 <div className="pr-6 z-20 xl:h-72">
                     {article.fields.title && <h4 className="mb-6">{article.fields.title}</h4>}
                     {article.fields.excerpt && <p>{`${article.fields.excerpt.substring(0, 100)}...`}</p>}
                     {article.fields.slug && <a href={`/articles/${article.fields.slug}`} passHref className="flex items-center my-6"><div className="w-3 h-px bg-vert-blue mr-2"></div>Read more</a>}
                 </div>
-                <div className="w-full bg-vert-green-light z-20" style={{ height: '2px'}}></div>
+                <div className="w-full bg-vert-green-light z-20" style={{ height: '2px' }}></div>
                 <div className="flex items-center my-6 text-sm text-#002B56">
-                    {article.fields.writer && <p>{article.fields.writer}</p>}
+                    {article.fields.author.fields.title && <p>{article.fields.author.fields.title}</p>}
                     <div className="h-1 w-1 rounded-full bg-#1B6AE3 mx-2"></div>
                     {article.fields.date && <p>{new Date(article.fields.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>}
                 </div>
--- components/Utilities/Share.js
+++ components/Utilities/Share.js
@@ -1,15 +1,29 @@
 import ShareItem from './ShareItem'
+import { useEffect, useState } from 'react'
 
 const Share = ({ links }) => {
+
+    const [socialLinks, setSocialLinks] = useState(links);
+
+    useEffect(() => {
+        const tempLinks = { ...links };
+        tempLinks.facebook = tempLinks.facebook.replace("https://vertrical.com", window.location.href);
+        tempLinks.twitter = tempLinks.twitter.replace("https://vertrical.com", window.location.href);
+        tempLinks.linkedIn = tempLinks.linkedIn.replace("https://vertrical.com", window.location.href);
+        tempLinks.whatsapp = tempLinks.whatsapp.replace("https://vertrical.com", window.location.href);
+        tempLinks.mail = tempLinks.mail.replace("https://vertrical.com", window.location.href);
+        setSocialLinks(tempLinks);
+    }, [links])
+
     return (
         <div className="flex flex-col lg:flex-row justify-center items-center mb-12">
             <p className="text-vert-green mb-4 lg:mb-0 lg:mr-2">Share</p>
             <div className="flex space-x-2">
-                {links.facebook && links.facebook !== '' && <ShareItem link={links.facebook} image={'/facebook.svg'} background={'#3B5998'} />}
-                {links.twitter && links.twitter !== '' && <ShareItem link={links.twitter} image={'/twitter.svg'} background={'#00ACEE'} />}
-                {links.linkedIn && links.linkedIn !== '' && <ShareItem link={links.linkedIn} image={'/linkedin.svg'} background={'#0072B1'} />}
-                {links.whatsapp && links.whatsapp !== '' && <ShareItem link={links.whatsapp} image={'/whatsapp.svg'} background={'#4AC959'} />}
-                {links.mail && links.mail !== '' && <ShareItem link={links.mail} image={'/email.svg'} background={'#FF8A47'} />}
+                {socialLinks.facebook && socialLinks.facebook !== '' && <ShareItem link={socialLinks.facebook} image={'/facebook.svg'} background={'#3B5998'} />}
+                {socialLinks.twitter && socialLinks.twitter !== '' && <ShareItem link={socialLinks.twitter} image={'/twitter.svg'} background={'#00ACEE'} />}
+                {socialLinks.linkedIn && socialLinks.linkedIn !== '' && <ShareItem link={socialLinks.linkedIn} image={'/linkedin.svg'} background={'#0072B1'} />}
+                {socialLinks.whatsapp && socialLinks.whatsapp !== '' && <ShareItem link={socialLinks.whatsapp} image={'/whatsapp.svg'} background={'#4AC959'} />}
+                {socialLinks.mail && socialLinks.mail !== '' && <ShareItem link={socialLinks.mail} image={'/email.svg'} background={'#FF8A47'} />}
             </div>
         </div>
     )
